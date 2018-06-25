1a74d9e69dd04c310ff113a5190ac961b58b12f7
saurookadook <maskiella@gmail.com>
2018-06-25 14:33:21 -0400

    Begin planning out app and begin work on board

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route } from 'react-router-dom'; // Switch?
 import  NavBar  from './components/NavBar.js';
 import { Home } from './components/Home.js';
 import { connect } from 'react-redux';
@@ -10,15 +10,24 @@ import { bindActionCreators } from 'redux';
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <Router>
+        <div className="App">
+          // <Switch> ?
+          <NavBar />
+          <Route exact path="/" component={Home} />
+          <Route exact path="/games" />
+          // </ Switch>
+        </div>
+      </Router>
+      // <div className="App">
+      //   <header className="App-header">
+      //     <img src={logo} className="App-logo" alt="logo" />
+      //     <h1 className="App-title">Welcome to React</h1>
+      //   </header>
+      //   <p className="App-intro">
+      //     To get started, edit <code>src/App.js</code> and save to reload.
+      //   </p>
+      // </div>
     );
   }
 }
