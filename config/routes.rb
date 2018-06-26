a1f98576f3a4e96003ff570dec82806207b6c88f
saurookadook <maskiella@gmail.com>
2018-06-25 20:11:39 -0400

    Continue work on board and initial state

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :games
 
-  # root to: 'welcome#home'
+  root to: 'welcome#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
